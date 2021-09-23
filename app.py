41e1a16bb0cda9e6efae37ecff281fbb620927a4
Ashok Kumar S <56039882+ashokkumars01@users.noreply.github.com>
2021-09-23 11:32:53 +0530

    Update app.py

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ except Exception as e:
 app = Flask(__name__)
 
 # Configure db
-'''try:
+try:
     logging.info("Configure Database")
     db = yaml.load(open('db.yaml'))
     app.config['MYSQL_HOST'] = db['mysql_host']
@@ -31,7 +31,7 @@ except Exception as e:
     print(e)
     logging.exception(e)
 
-mysql = MySQL(app)'''
+mysql = MySQL(app)
 
 @app.route('/')
 def home():
@@ -51,10 +51,10 @@ def predict():
                 Label = 'ham'
             elif my_prediction == 1:
                 Label = 'spam'
-            #mycursor = mysql.connection.cursor()
-            #mycursor.execute("INSERT INTO prediction(Label, Message) VALUES(%s, %s)",(Label, Message))
-            #mysql.connection.commit()
-            #mycursor.close()
+            mycursor = mysql.connection.cursor()
+            mycursor.execute("INSERT INTO prediction(Label, Message) VALUES(%s, %s)",(Label, Message))
+            mysql.connection.commit()
+            mycursor.close()
         return render_template('result.html',prediction = my_prediction)
     except Exception as e:
         print(e)
